ca5bcc8b28f6a067973ff6f83d167531e6dfea09
Sam Pegler <jobs@sampegler.co.uk>
2016-11-18 16:28:32 +0000

    Quick pass at Python3 upgrade.

--- opentsdb_pandas/response.py
+++ opentsdb_pandas/response.py
@@ -1,138 +1,142 @@
+"""Response Module."""
 import pandas
+import logging
 
 try:
     # Use ujson if available.
     import ujson as json
-except Exception:
+except ImportError:
     import json
 
+log = logging.getLogger(__name__)
 
-class OpenTSDBResponseSerie(object):
-    """
-        A single OpenTSDB response serie i.e 1 element of the response
-        array.
 
-        Params:
-            **kwargs : OpenTSDB response serie data
+class OpenTSDBResponseSeries():
+    """A single OpenTSDB response series i.e 1 element of the response array.
+
+    Params:
+        **kwargs : OpenTSDB response series data
     """
+
     def __init__(self, **kwargs):
-        for k,v in kwargs.items():
+        """Initialisation for OpenTSDBResponseSeries."""
+        for k, v in kwargs.items():
             setattr(self, k, v)
-    
+
     @property
     def id(self):
         """
-            id for serie 
+        Id for series.
 
-            Returns:
-                metric{sorted=tag,key=value}
+        Returns:
+            metric{sorted=tag,key=value}
         """
         if len(self.tags.keys()) > 0:
-            tags = ",".join(["%s=%s" % 
-                (k, self.tags[k]) for k in sorted(self.tags.keys())])
+            tags = ",".join(["%s=%s" %
+                             (k, self.tags[k]) for k in
+                             sorted(self.tags.keys())])
             return "%s{%s}" % (self.metric, tags)
         else:
             return self.metric
 
-
-    def alias(self, functOrStr):
+    def alias(self, func_or_str):
         """
-            User specified alias using lambda functions and string formatting using
-            metadata provided by opentsdb.
-            This function fails silently.
+        Generate aliases from OpenTSDB.
 
-            Params:
-                functOrStr :    lambda function or python string format. When using lambda
-                                functions,  they must begin with '!' e.g. !lambda x: x....
-            Return:
-                Formatted alias on success and id or failure.
+        User specified alias using lambda functions and string formatting
+        using metadata provided by opentsdb.
+        # TODO: Don't fail silently.
+        This function fails silently.
+
+        Args:
+            func_or_str:    lambda function or python string format. When
+                            using lambda functions,  they must begin with
+                            '!' e.g. !lambda x: x....
+        Return:
+            Formatted alias on success and id or failure.
         """
-        flatData = self.__flattenedMetadata()
+        flat_data = self.__flattenedMetadata()
         # Normalized alias
         _alias = ""
-        if functOrStr.startswith("!"):
+        if func_or_str.startswith("!"):
             try:
-                _alias = eval(functOrStr[1:])(flatData)
-            except Exception, e:
-                pass
+                _alias = eval(func_or_str[1:])(flat_data)
+            except Exception as e:
+                log.info("Caught an exception generating alias. %r" % (e))
         else:
             try:
-                _alias = functOrStr % (flatData)
-            except Exception, e:
-                pass
-        
+                _alias = func_or_str % (flat_data)
+            except Exception as e:
+                log.info("Caught an exception generating alias. %r" % (e))
+
         if _alias == "":
             return self.id
 
         return _alias
 
-
-    def __flattenedMetadata(self):
-        """ 
-            Flattens all metadata which is used for normalization 
-        """
+    def __flattened_metadata(self):
+        """Flatten all metadata which is used for normalization."""
         return dict([("metric", self.metric)] +
-            [("tags.%s" % (k), v) for k, v in self.tags.items()])
+                    [("tags.%s" % (k), v) for k, v in self.tags.items()])
 
-    def datapoints(self, convertTime=False):
-        """ 
-            Converts datapoints
-
-            Params:
-                convertTime : Whether to convert epoch to pandas datetime
-
-            Return:
-                Array of tuples (time, value)
+    def datapoints(self, convert_time=False):
         """
-        if convertTime:
-            return dict([(pandas.to_datetime(int(k), unit='s'), v) for k, v in self.dps.items()])
-                
+        Convert datapoints.
+
+        KWargs:
+            convert_time: Whether to convert epoch to pandas datetime
+
+        Return:
+            Array of tuples (time, value)
+        """
+        if convert_time:
+            return dict([(pandas.to_datetime(int(k), unit='s'), v)
+                        for k, v in self.dps.items()])
+
         return dict([(int(k), v) for k, v in self.dps.items()])
 
 
 class OpenTSDBResponse(object):
-    """ Complete OpenTSDB response """
+    """Complete OpenTSDB response."""
 
-    def __init__(self, otsdbResp):
+    def __init__(self, otsdb_resp):
+        """Initialise an OpenTSDBResponse.
+
+        Args:
+            otsdb_resp: raw opentsdb response as a str, list or tuple.
         """
-            Params:
-                otsdbResp : raw opentsdb response as a str, list or tuple.
-        """
-        if isinstance(otsdbResp, str) or isinstance(otsdbResp, unicode):
-            # string response       
-            self._series = [ OpenTSDBResponseSerie(**s) for s in json.loads(otsdbResp) ]
-        elif isinstance(otsdbResp, list) or isinstance(otsdbResp, tuple):
+        if isinstance(otsdb_resp, str) or isinstance(otsdb_resp, unicode):
+            # string response
+            self._series = [OpenTSDBResponseSeries(
+                **s) for s in json.loads(otsdb_resp)]
+        elif isinstance(otsdb_resp, list) or isinstance(otsdb_resp, tuple):
             # dict response
-            self._series = [ OpenTSDBResponseSerie(**s) for s in otsdbResp ]
+            self._series = [OpenTSDBResponseSeries(**s) for s in otsdb_resp]
         else:
-            raise RuntimeError("Invalid type: %s" % (type(otsdbResp)))
-
+            raise RuntimeError("Invalid type: %s" % (type(otsdb_resp)))
 
     @property
     def series(self):
-        """ 
-            Use iterator for better memory management 
-        """
+        """Use iterator for better memory management."""
         for s in self._series:
             yield s
 
-
-    def DataFrame(self, aliasTransform=None, convertTime=False):
+    def data_frame(self, alias_transform=None, convert_time=False):
         """
-            Converts an OpenTSDB array response into a DataFrame
+        Convert an OpenTSDB array response into a DataFrame.
 
-            Params:
-                convertTime : Whether to convert epoch to pandas datetime
-                aliasTransform : lambda function or string format to customize
-                                 serie name i.e. alias
+        KWargs:
+            alias_transform: lambda function or string format to customize
+                             serie name i.e. alias
+            convert_time: Whether to convert epoch to pandas datetime
 
-            Return:
-                OpenTSDB response DataFrame
+        Return:
+            OpenTSDB response DataFrame
         """
-        if aliasTransform == None:
+        if alias_transform is None:
             return pandas.DataFrame(dict([
-                (s.id, s.datapoints(convertTime)) for s in self.series ]))
+                (s.id, s.datapoints(convert_time)) for s in self.series]))
         else:
             return pandas.DataFrame(dict([
-                (s.alias(aliasTransform), s.datapoints(convertTime)) for s in self.series ]))
-
+                (s.alias(alias_transform), s.datapoints(convert_time))
+                for s in self.series]))
